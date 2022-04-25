5fd23d410dbe9661d1561d831a377ba0a93f36bb
Vanessa Rivera <62850023+realstorml0rd@users.noreply.github.com>
2022-04-24 20:34:10 -0400

    Update Customer.java
    added the rest of the fields to customer object

--- src/main/java/com/vanessademoapi/model/Customer.java
+++ src/main/java/com/vanessademoapi/model/Customer.java
@@ -13,18 +13,39 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
+    
     @Column(nullable = false, unique = true)
     private String firstName;
 
-    @Column(nullable = false)
-    private Integer age;
+    @Column(nullable = false, unique = true)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String emailAddress;
+
+    @Column(nullable = false, unique = true)
+    private Long phoneNumber;
+
+    @Column(nullable = true, unique = true)
+    private String streetAddress;
+
+    @Column(nullable = false, unique = false)
+    private String city;
+
+    @Column(nullable = false, unique = false)
+    private String state;
+
+    @Column(nullable = false,unique = false)
+    private Integer zipCode;
+
+    @Column(nullable = false, unique = false)
+    private String businessType;
 
     public Customer(String firstName, Integer age) {
 
         super();
         this.firstName = firstName;
-        this.age = age;
+
 
     }
 
@@ -32,7 +53,21 @@ public class Customer {
         return firstName;
     }
 
-    public Integer getAge() { return age; }
+    public String getLastName() {return lastName; }
+
+    public String getEmailAddress() {return emailAddress; }
+
+    public Long getPhoneNumber() {return phoneNumber; }
+
+    public String getStreetAddress() {return streetAddress; }
+
+    public String getCity() {return city; }
+
+    public String getState() {return state; }
+
+    public Integer getZipCode() {return zipCode; }
+
+    public String getBusinessType() {return businessType; }
 
     public long getId() { return id; }
 }
